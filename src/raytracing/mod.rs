45e73dab471cc1b209201abcecdfa711983233c8
Therdel <14974231+Therdel@users.noreply.github.com>
2021-04-05 23:15:07 +0200

    organize primitives into module

--- src/raytracing/mod.rs
+++ src/raytracing/mod.rs
@@ -3,17 +3,11 @@ pub use intersect::*;
 
 mod intersect_tests;
 
-mod sphere;
-pub use sphere::*;
-
-mod plane;
-pub use plane::*;
-
-mod triangle;
-pub use triangle::*;
-
 mod ray;
 pub use ray::*;
 
 mod hitpoint;
 pub use hitpoint::*;
+
+mod primitives;
+pub use primitives::*;
---
+++ src/raytracing/primitives/mod.rs
@@ -0,0 +1,7 @@
+mod plane;
+mod sphere;
+mod triangle;
+
+pub use plane::*;
+pub use sphere::*;
+pub use triangle::*;

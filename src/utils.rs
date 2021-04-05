fea1bce8dbb624d4bd6ce0bfc60e4b02e3a4c758
Therdel <14974231+Therdel@users.noreply.github.com>
2021-04-05 23:17:01 +0200

    ray equation

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,10 @@
+use crate::raytracing::Ray;
+
 #[cfg(test)]
 pub fn assert_approx_eq(a: f32, b: f32) {
     float_eq::assert_float_eq!(a, b, rmax <= 2.0 * f32::EPSILON)
+}
+
+pub fn ray_equation(ray: &Ray, t: f32) -> glm::Vec3 {
+    ray.origin + ray.direction * t
 }

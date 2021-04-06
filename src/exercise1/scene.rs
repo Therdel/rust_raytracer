9899ad672967bc6fd29ec161f22458b3866d374b
Therdel <14974231+Therdel@users.noreply.github.com>
2021-04-06 15:43:22 +0200

    Scene container and intersection test

--- src/exercise1/scene.rs
+++ src/exercise1/scene.rs
@@ -1,12 +1,39 @@
-use crate::raytracing::{self, Ray, Hitpoint};
+use crate::raytracing::{self, Ray, Hitpoint, Sphere, Plane, Triangle};
 
 pub struct Scene {
+    pub planes: Vec<Plane>,
+    pub spheres: Vec<Sphere>,
+    pub triangles: Vec<Triangle>,
 }
 
 impl raytracing::Intersect for Scene {
     type Result = Hitpoint;
 
-    fn intersect(&self, ray: &Ray) -> Option<Hitpoint> {
-        todo!()
+    fn intersect(&self, ray: &Ray) -> Option<Self::Result> {
+        let mut closest_hitpoint: Option<Self::Result> = None;
+
+        let mut check_hitpoint = |hitpoint: Option<Self::Result>| {
+            if let Some(hitpoint) = hitpoint {
+                if let Some(ref mut closest_hitpoint) = closest_hitpoint {
+                    if hitpoint.t < closest_hitpoint.t {
+                        *closest_hitpoint = hitpoint;
+                    }
+                } else {
+                    closest_hitpoint = Some(hitpoint);
+                }
+            }
+        };
+
+        for plane in &self.planes {
+            check_hitpoint(plane.intersect(ray));
+        }
+        for sphere in &self.spheres {
+            check_hitpoint(sphere.intersect(ray));
+        }
+        for triangle in &self.triangles {
+            check_hitpoint(triangle.intersect(ray));
+        }
+
+        closest_hitpoint
     }
 }

22b63aff0b0329770ded54eaa6ae9c1f79af3ed2
Therdel <14974231+Therdel@users.noreply.github.com>
2021-04-26 00:01:00 +0200

    more 3D-ish test scene rendered with raytracing

--- src/main.rs
+++ src/main.rs
@@ -27,14 +27,14 @@ fn main() -> std::io::Result<()> {
     scene.spheres = test_spheres(&scene.materials);
     scene.triangles = test_triangles(&scene.materials);
 
-    let mut canvas = Canvas::new((IMAGE_WIDTH, IMAGE_HEIGHT), Color::urple());
+    let mut canvas = Canvas::new((IMAGE_WIDTH, IMAGE_HEIGHT), Color::black());
     let raytracer = Raytracer::new(&scene);
 
     for y in 0..IMAGE_HEIGHT {
         for x in 0..IMAGE_WIDTH {
             let coordinate = glm::vec2(x as _, y as _);
             let ray = raytracer.generate_primary_ray(&coordinate);
-            if let Some(hit_color) = raytracer.depth_map(&ray) {
+            if let Some(hit_color) = raytracer.raytrace(&ray) {
                 canvas.set_pixel(x, y, &hit_color);
             }
         }
@@ -46,10 +46,10 @@ fn main() -> std::io::Result<()> {
 fn test_lights() -> Vec<Light> {
     vec![
         Light {
-            position: glm::vec4(1.0, 1.0, 1.0, 0.0), // directional
+            position: glm::vec4(1.0, 5.0, 1.0, 1.0), // directional
             color: LightColor {
                 ambient: glm::vec3(0.1, 0.1, 0.1),
-                diffuse: glm::vec3(0.2, 0.2, 0.2),
+                diffuse: glm::vec3(0.5, 0.5, 0.5),
                 specular: glm::vec3(0.5, 0.5, 0.5),
             }
         }
@@ -60,7 +60,7 @@ fn test_camera(width: usize, height: usize) -> Camera {
     Camera {
         position: glm::vec3(0.0, 0.0, -1.0),
         orientation: glm::vec3(0.0f32.to_radians(),
-                               0.0f32.to_radians(),
+                               20.0f32.to_radians(),
                                0.0f32.to_radians()),
         pixel_width: width,
         pixel_height: height,
@@ -105,7 +105,7 @@ fn test_triangles(materials: &[Material]) -> Vec<Triangle> {
             glm::vec3(5.0, -2.5, -5.0),
             glm::vec3(0.0, 2.5, -5.0),
             materials.iter().find(|&material| {
-                material.name == "some_shiny_red"
+                material.name == "some_shiny_yellow"
             }).unwrap()
         )
     ]
@@ -117,7 +117,7 @@ fn test_planes(materials: &[Material]) -> Vec<Plane> {
             normal: glm::vec3(0.0, -1.0, 0.0),
             distance: 5.0,
             material: materials.iter().find(|&material| {
-                material.name == "some_shiny_red"
+                material.name == "some_shiny_green"
             }).unwrap()
         }
     ]
@@ -126,10 +126,24 @@ fn test_planes(materials: &[Material]) -> Vec<Plane> {
 fn test_spheres(materials: &[Material]) -> Vec<Sphere> {
     vec![
         Sphere {
-            center: glm::vec3(0.0, 0.0, -2.0),
+            center: glm::vec3(0.0, 1.0, -5.0),
             radius: 0.5,
             material: materials.iter().find(|&material| {
-                material.name == "some_shiny_yellow"
+                material.name == "some_shiny_red"
+            }).unwrap()
+        },
+        Sphere {
+            center: glm::vec3(0.0, 0.0, -4.0),
+            radius: 0.5,
+            material: materials.iter().find(|&material| {
+                material.name == "some_shiny_red"
+            }).unwrap()
+        },
+        Sphere {
+            center: glm::vec3(0.0, -1.0, -3.0),
+            radius: 0.5,
+            material: materials.iter().find(|&material| {
+                material.name == "some_shiny_red"
             }).unwrap()
         }
     ]

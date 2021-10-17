44a9062a62ba205745e3f17ae66f401cd44268ea
Therdel <14974231+Therdel@users.noreply.github.com>
2021-10-17 18:30:28 +0100

    move AliasRc to own file

--- lib_raytracer/src/utils/alias_rc.rs
+++ lib_raytracer/src/utils/alias_rc.rs
@@ -1,29 +1,5 @@
-use crate::raytracing::{Ray, Hitpoint};
-use nalgebra_glm as glm;
 use std::rc::Rc;
 use std::ops::Deref;
-
-#[cfg(test)]
-pub fn assert_approx_eq(a: f32, b: f32) {
-    float_eq::assert_float_eq!(a, b, rmax <= 2.0 * f32::EPSILON)
-}
-
-pub fn ray_equation(ray: &Ray, t: f32) -> glm::Vec3 {
-    ray.origin + ray.direction * t
-}
-
-pub fn take_hitpoint_if_closer<'material>(closest_hitpoint: &mut Option<Hitpoint>,
-                                          hitpoint: Option<Hitpoint>) {
-    if let Some(hitpoint) = hitpoint {
-        if let Some(ref mut closest_hitpoint) = closest_hitpoint {
-            if hitpoint.t < closest_hitpoint.t {
-                *closest_hitpoint = hitpoint;
-            }
-        } else {
-            *closest_hitpoint = Some(hitpoint);
-        }
-    }
-}
 
 /// A safe version of C++ std::shared_ptr's aliasing constructor for Rust [`std::Rc<T>`][std::Rc].
 /// Derefs to a borrow of the Rc's content by caching a user-generated pointer.
@@ -59,7 +35,7 @@ impl<Parent, Child> AliasRc<Parent, Child>
                child_from_parent: impl Fn(&Parent)->&Child) -> Self {
         let child = child_from_parent(parent.deref()) as _;
 
-        AliasRc {
+        Self {
             parent,
             child
         }
@@ -97,7 +73,7 @@ impl<Parent, Child> Clone for AliasRc<Parent, Child>
         Parent: ?Sized,
         Child: ?Sized {
     fn clone(&self) -> Self {
-        AliasRc {
+        Self {
             parent: self.parent.clone(),
             child: self.child
         }
---
+++ lib_raytracer/src/utils/mod.rs
@@ -0,0 +1,27 @@
+mod alias_rc;
+pub use alias_rc::*;
+
+use crate::raytracing::{Ray, Hitpoint};
+use nalgebra_glm as glm;
+
+#[cfg(test)]
+pub fn assert_approx_eq(a: f32, b: f32) {
+    float_eq::assert_float_eq!(a, b, rmax <= 2.0 * f32::EPSILON)
+}
+
+pub fn ray_equation(ray: &Ray, t: f32) -> glm::Vec3 {
+    ray.origin + ray.direction * t
+}
+
+pub fn take_hitpoint_if_closer<'material>(closest_hitpoint: &mut Option<Hitpoint>,
+                                          hitpoint: Option<Hitpoint>) {
+    if let Some(hitpoint) = hitpoint {
+        if let Some(ref mut closest_hitpoint) = closest_hitpoint {
+            if hitpoint.t < closest_hitpoint.t {
+                *closest_hitpoint = hitpoint;
+            }
+        } else {
+            *closest_hitpoint = Some(hitpoint);
+        }
+    }
+}

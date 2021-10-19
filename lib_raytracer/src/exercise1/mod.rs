c446ba95f8abe82c7d872fdde3606357ad084d1f
Therdel <14974231+Therdel@users.noreply.github.com>
2021-10-19 14:36:44 +0100

    introduce SceneFileParser and MeshLoader

--- lib_raytracer/src/exercise1/mod.rs
+++ lib_raytracer/src/exercise1/mod.rs
@@ -5,3 +5,6 @@ mod canvas;
 pub use canvas::*;
 
 pub mod object_file;
+
+mod scene_file_parser;
+pub use scene_file_parser::{SceneFileParser, MeshLoader};
---
+++ lib_raytracer/src/exercise1/scene_file_parser/mod.rs
@@ -0,0 +1,52 @@
+use std::io::{self, BufRead, BufReader};
+use std::path::{Path, PathBuf};
+
+use crate::exercise1::object_file::{self, WindingOrder};
+use crate::exercise1::Scene;
+use crate::raytracing::{Material, Mesh};
+use crate::utils::AliasArc;
+
+fn get_material(materials: AliasArc<Vec<Material>, [Material]>, name: &str) -> Option<AliasArc<Vec<Material>, Material>> {
+    let index = materials
+        .iter()
+        .enumerate()
+        .find(|&(_, material)| {
+            material.name == name
+        })
+        .map(|(index, _)| index)?;
+
+    let materials_arc = AliasArc::into_parent(materials);
+    let alias_arc = AliasArc::new(materials_arc, |vec| &vec[index]);
+    Some(alias_arc)
+}
+
+fn get_mesh(meshes: AliasArc<Vec<Mesh>, [Mesh]>, name: &str) -> Option<AliasArc<Vec<Mesh>, Mesh>> {
+    let index = meshes
+        .iter()
+        .enumerate()
+        .find(|&(_, mesh)| {
+            mesh.name == name
+        })
+        .map(|(index, _)| index)?;
+
+    let mesh_arc = AliasArc::into_parent(meshes);
+    let alias_arc = AliasArc::new(mesh_arc, |vec| &vec[index]);
+    Some(alias_arc)
+}
+
+pub trait MeshLoader {
+    fn load(&self, name: &str, file_name: &str, material: AliasArc<Vec<Material>, Material>,
+            winding_order: WindingOrder) -> io::Result<Mesh>;
+}
+
+pub struct SceneFileParser<S: BufRead, M: MeshLoader> {
+    pub file_reader: S,
+    pub mesh_loader: M,
+}
+
+impl<S: BufRead, M: MeshLoader> SceneFileParser<S, M> {
+    pub fn parse_json(&mut self) -> io::Result<Scene> {
+        todo!()
+    }
+}
+

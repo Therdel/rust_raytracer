c21033374e44a5201a6ffe5646ae53bae6955ea3
Therdel <14974231+Therdel@users.noreply.github.com>
2021-10-25 04:56:39 +0100

    code cleanup

--- wasm_interface/src/lib.rs
+++ wasm_interface/src/lib.rs
@@ -7,35 +7,16 @@ use wasm_bindgen::prelude::*;
 use lib_raytracer::exercise1::scene_file::Parser;
 use lib_raytracer::raytracing::{color::*, raytracer::{Public, Raytracer}, Screen};
 
+use crate::color::{ColorRgbaU8, QuantizeToU8};
 use crate::fake_same_mesh_loader::FakeSameMeshLoader;
 
+mod color;
 mod fake_same_mesh_loader;
 
 // Called when the wasm module is instantiated
 #[wasm_bindgen(start)]
 pub fn main() -> Result<(), JsValue> {
     Ok(())
-}
-
-pub type ColorRgba = glm::Vec4;
-pub type ColorRgbaU8 = [u8; 4]; // TODO: replace with glm::U8Vec4?
-
-pub trait QuantizeToU8 {
-    fn quantize(&self) -> ColorRgbaU8;
-}
-
-impl QuantizeToU8 for ColorRgba {
-    fn quantize(&self) -> ColorRgbaU8 {
-        let mut clamped_color = glm::clamp(self, 0.0, 1.0);
-        clamped_color = clamped_color * 255.;
-
-        [
-            clamped_color.x as u8,
-            clamped_color.y as u8,
-            clamped_color.z as u8,
-            clamped_color.w as u8
-        ]
-    }
 }
 
 #[wasm_bindgen]
@@ -51,7 +32,7 @@ pub fn render(canvas_u8: &mut [u8], width: usize, height: usize,
     scene.screen = Screen {
         pixel_width: width,
         pixel_height: height,
-        background: Color::urple()
+        background: Color::urple(),
     };
 
     let raytracer = Raytracer::new(&scene);

810e000f9bd11ab8bb88c52431f3f2159de3b507
Therdel <14974231+Therdel@users.noreply.github.com>
2021-11-02 21:09:59 +0000

    wasm: sepearate scene parsing and BVH generation from rendering

--- wasm_interface/src/lib.rs
+++ wasm_interface/src/lib.rs
@@ -22,38 +22,56 @@ pub fn main() -> Result<(), JsValue> {
 }
 
 #[wasm_bindgen]
-pub fn render(canvas_u8: &mut [u8], width: usize, height: usize,
-              scene: &[u8], mesh_obj: &[u8]) {
-    let canvas_raw_color = canvas_u8.as_mut_ptr() as *mut ColorRgbaU8;
-    let canvas = unsafe { slice::from_raw_parts_mut(canvas_raw_color, width * height) };
+pub struct Renderer {
+    width: usize,
+    height: usize,
+    raytracer: Raytracer,
+}
 
-    let mut scene = Parser {
-        file_reader: Cursor::new(scene),
-        mesh_loader: FakeSameMeshLoader { mesh_obj },
-    }.parse_json().unwrap();
-    scene.screen = Screen {
-        pixel_width: width,
-        pixel_height: height,
-        background: Color::urple(),
-    };
+#[wasm_bindgen]
+impl Renderer {
+    #[wasm_bindgen(constructor)]
+    pub fn new(width: usize, height: usize,
+               scene: &[u8], mesh_obj: &[u8]) -> Self {
+        let mut scene = Parser {
+            file_reader: Cursor::new(scene),
+            mesh_loader: FakeSameMeshLoader { mesh_obj },
+        }.parse_json().unwrap();
+        scene.screen = Screen {
+            pixel_width: width,
+            pixel_height: height,
+            background: Color::urple(),
+        };
+        let raytracer = Raytracer::new(scene);
 
-    let raytracer = Raytracer::new(&scene);
-    for y in 0..height {
-        for x in 0..width {
-            let coordinate = glm::vec2(x as _, y as _);
-            let ray = raytracer.generate_primary_ray(&coordinate);
+        Renderer {
+            width,
+            height,
+            raytracer,
+        }
+    }
 
-            let color = match raytracer.raytrace(&ray) {
-                Some(hit_color) => hit_color,
-                None => scene.screen.background
-            };
-            let color = glm::vec4(color.x, color.y, color.z, 1.0);
+    pub fn render(&self, canvas_u8: &mut [u8]) {
+        let canvas_raw_color = canvas_u8.as_mut_ptr() as *mut ColorRgbaU8;
+        let canvas = unsafe { slice::from_raw_parts_mut(canvas_raw_color, self.width * self.height) };
 
-            let max_y_index = height - 1;
-            let y_inverted = max_y_index - y;
-            let offset = x + width * y_inverted;
+        for y in 0..self.height {
+            for x in 0..self.width {
+                let coordinate = glm::vec2(x as _, y as _);
+                let ray = self.raytracer.generate_primary_ray(&coordinate);
 
-            canvas[offset] = color.quantize();
+                let color = match self.raytracer.raytrace(&ray) {
+                    Some(hit_color) => hit_color,
+                    None => Color::urple(), //scene.screen.background
+                };
+                let color = glm::vec4(color.x, color.y, color.z, 1.0);
+
+                let max_y_index = self.height - 1;
+                let y_inverted = max_y_index - y;
+                let offset = x + self.width * y_inverted;
+
+                canvas[offset] = color.quantize();
+            }
         }
     }
 }

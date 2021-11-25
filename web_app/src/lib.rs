9ff589b73f565410d34fb1c7be37961a4af2c368
Therdel <14974231+Therdel@users.noreply.github.com>
2021-11-25 20:22:51 +0000

    refactor renderer out of lib

--- web_app/src/lib.rs
+++ web_app/src/lib.rs
@@ -1,108 +1,13 @@
-use std::io::Cursor;
-use std::slice;
-
-use nalgebra_glm as glm;
 use wasm_bindgen::prelude::*;
-
-use lib_raytracer::exercise1::scene_file::Parser;
-use lib_raytracer::raytracing::{color::*, raytracer::{Public, Raytracer}, Screen};
-
-use crate::color::{ColorRgbaU8, QuantizeToU8};
-use crate::fake_same_mesh_loader::FakeSameMeshLoader;
 
 mod color;
 mod fake_same_mesh_loader;
 mod utils;
+mod renderer;
 
 // Called when the wasm module is instantiated
 #[wasm_bindgen(start)]
 pub fn main() -> Result<(), JsValue> {
     utils::set_panic_hook();
     Ok(())
-}
-
-#[wasm_bindgen]
-pub struct Renderer {
-    width: usize,
-    height: usize,
-    raytracer: Raytracer,
-}
-
-#[wasm_bindgen]
-impl Renderer {
-    #[wasm_bindgen(constructor)]
-    pub fn new(width: usize, height: usize,
-               scene: &[u8], mesh_obj: &[u8]) -> Self {
-        let mut scene = Parser {
-            file_reader: Cursor::new(scene),
-            mesh_loader: FakeSameMeshLoader { mesh_obj },
-        }.parse_json().unwrap();
-        scene.screen = Screen {
-            pixel_width: width,
-            pixel_height: height,
-            background: Color::urple(),
-        };
-        let raytracer = Raytracer::new(scene);
-
-        Renderer {
-            width,
-            height,
-            raytracer,
-        }
-    }
-
-    pub fn render(&self, canvas_u8: &mut [u8]) {
-        let canvas = self.canvas_from_raw(canvas_u8);
-
-        for y in 0..self.height {
-            for x in 0..self.width {
-                self.render_pixel(canvas, x, y);
-            }
-        }
-    }
-
-    pub fn render_interlaced(&self, canvas_u8: &mut [u8], y_offset: usize, row_jump: usize) {
-        let canvas = self.canvas_from_raw(canvas_u8);
-
-        for y in (y_offset..self.height).step_by(row_jump) {
-            for x in 0..self.width {
-                self.render_pixel(canvas, x, y);
-            }
-        }
-    }
-
-    fn render_pixel(&self, canvas: &mut [ColorRgbaU8], x: usize, y: usize) {
-        let max_y_index = self.height - 1;
-        let y_inverted = max_y_index - y;
-
-        let coordinate = glm::vec2(x as _, y_inverted as _);
-        let ray = self.raytracer.generate_primary_ray(&coordinate);
-
-        let color = match self.raytracer.raytrace(&ray) {
-            Some(hit_color) => hit_color,
-            None => Color::urple(), //scene.screen.background
-        };
-        let color = glm::vec4(color.x, color.y, color.z, 1.0);
-        let offset = x + self.width * y;
-
-        canvas[offset] = color.quantize();
-    }
-
-    pub fn resize_screen(&mut self, width: usize, height: usize) {
-        self.width = width;
-        self.height = height;
-        self.raytracer.resize_screen(width, height)
-    }
-
-    pub fn turn_camera(&mut self, drag_begin_x: f32, drag_begin_y: f32, drag_end_x: f32, drag_end_y: f32) {
-        self.raytracer.turn_camera(&glm::vec2(drag_begin_x, drag_begin_y),
-                                                   &glm::vec2(drag_end_x, drag_end_y));
-    }
-
-    fn canvas_from_raw(&self, canvas_u8: &mut [u8]) -> &mut [ColorRgbaU8] {
-        let canvas_raw_color = canvas_u8.as_mut_ptr() as *mut ColorRgbaU8;
-        unsafe {
-            slice::from_raw_parts_mut(canvas_raw_color, self.width * self.height)
-        }
-    }
 }
--- web_app/src/utils.rs
+++ web_app/src/utils.rs
@@ -1,3 +1,9 @@
+use std::slice;
+
+use wasm_bindgen::prelude::*;
+
+use crate::color::ColorRgbaU8;
+
 pub fn set_panic_hook() {
     // When the `console_error_panic_hook` feature is enabled, we can call the
     // `set_panic_hook` function at least once during initialization, and then
@@ -7,4 +13,18 @@ pub fn set_panic_hook() {
     // https://github.com/rustwasm/console_error_panic_hook#readme
     #[cfg(feature = "console_error_panic_hook")]
     console_error_panic_hook::set_once();
+}
+
+pub fn canvas_from_raw(canvas_u8: &[u8]) -> &[ColorRgbaU8] {
+    let canvas_raw_color = canvas_u8.as_ptr() as *const ColorRgbaU8;
+    unsafe {
+        slice::from_raw_parts(canvas_raw_color, canvas_u8.len() / 4)
+    }
+}
+
+pub fn canvas_from_raw_mut(canvas_u8: &mut [u8]) -> &mut [ColorRgbaU8] {
+    let canvas_raw_color = canvas_u8.as_mut_ptr() as *mut ColorRgbaU8;
+    unsafe {
+        slice::from_raw_parts_mut(canvas_raw_color, canvas_u8.len() / 4)
+    }
 }

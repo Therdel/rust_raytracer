f6f4d83655ed5ccb84ebf02b6d6f20a33a5041ec
Therdel <14974231+Therdel@users.noreply.github.com>
2022-05-08 15:54:37 +0100

    wrap module initialization in own js file to see it in the firefox files list

--- src/test_worker_module.js
+++ src/test_worker_module.js
@@ -1,9 +1,10 @@
 
+import { test_module_import } from "./test_module.js";
 
-import {test_module_import} from "./test_module.js";
-
-onmessage = ({ data }) => {
-    postMessage(`hi from worker. Mainwindow said: [${data}]`);
-    postMessage(`Calling imported function from module worker: ${test_module_import()}`)
-    postMessage(`SharedArrayBuffer Test from worker: ${new SharedArrayBuffer(10)}`);
-}
+export function worker_init() {
+    onmessage = ({ data }) => {
+        postMessage(`hi from worker. Mainwindow said: [${data}]`);
+        postMessage(`Calling imported function from module worker: ${test_module_import()}`)
+        postMessage(`SharedArrayBuffer Test from worker: ${new SharedArrayBuffer(10)}`);
+    };
+};
---
+++ src/test_worker_module_proxy.js
@@ -0,0 +1,3 @@
+import {worker_init} from "./test_worker_module.js"
+
+worker_init();
